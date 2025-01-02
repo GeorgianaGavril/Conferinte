f60405c58c8501a56f0413bf8b6f53cb315ff3ed
GeorgianaGavril <georgianagavril401@gmail.com>
2025-01-02 02:20:51 +0200

    terminat in principiu sign in/up, mai trebuie legate si stilizate

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,12 +9,27 @@ export default function App() {
 }
 
 function Login() {
+  const [signUp, setSignUp] = useState(false);
+
+  return (
+    <div>
+      {signUp ? (
+        <SignUp toggle={() => setSignUp(false)} />
+      ) : (
+        <SignIn toggle={() => setSignUp(true)} />
+      )}
+    </div>
+  );
+}
+
+function SignUp({ toggle }) {
   const [name, setName] = useState("");
   const [firstName, setFirstName] = useState("");
   const [role, setRole] = useState("Organzitor");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
+  const [message, setMessage] = useState("");
 
   const [errors, setErrors] = useState({});
 
@@ -51,6 +66,9 @@ function Login() {
       const user = { name, firstName, role, email, password, confirmPassword };
       console.log(user);
 
+      setMessage("Te-ai înscris cu succes!");
+      setTimeout(() => setMessage(""), 3000);
+
       setName("");
       setFirstName("");
       setRole("Organizator");
@@ -64,91 +82,151 @@ function Login() {
   }
 
   return (
-    <div className="login">
-      <h2>Bine ai revenit!</h2>
-      <br></br>
-      <form onSubmit={handleSubmit}>
-        <div className="form-group">
-          {/* <label htmlFor="name">Nume:</label> */}
-          <input
-            className={`${errors.name ? "errors" : ""}`}
-            type="text"
-            id="name"
-            name="name"
-            placeholder="Nume"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-          {errors.name && <small>{errors.name}</small>}
+    <>
+      {message ? (
+        <div className="message">
+          {message}
+          <br></br>
         </div>
-        <div className="form-group">
-          {/* <label htmlFor="firstName">Prenume:</label> */}
-          <input
-            className={`${errors.firstName ? "errors" : ""}`}
-            type="text"
-            id="firstName"
-            name="firstName"
-            placeholder="Prenume"
-            value={firstName}
-            onChange={(e) => setFirstName(e.target.value)}
-          />
-          {errors.firstName && <small>{errors.firstName}</small>}
-        </div>
-        <div className="form-group">
-          {/* <label htmlFor="role">Selectați tipul de utilizator:</label> */}
-          <select
-            id="role"
-            name="role"
-            value={role}
-            onChange={(e) => setRole(e.target.value)}
-          >
-            <option>Organizator</option>
-            <option>Autor</option>
-            <option>Reviewer</option>
-          </select>
-        </div>
-        <div className="form-group">
-          {/* <label htmlFor="email">Email:</label> */}
-          <input
-            className={`${errors.email ? "errors" : ""}`}
-            type="email"
-            id="email"
-            name="email"
-            placeholder="Email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          {errors.email && <small>{errors.email}</small>}
-        </div>
-        <div className="form-group">
-          {/* <label htmlFor="password">Parolă:</label> */}
-          <input
-            className={`${errors.password ? "errors" : ""}`}
-            type="password"
-            id="password"
-            name="password"
-            placeholder="Parolă"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          {errors.password && <small>{errors.password}</small>}
-        </div>
-        <div className="form-group">
-          {/* <label htmlFor="confirmPassword">Confirmare parolă:</label> */}
-          <input
-            className={`${errors.confirmPassword ? "errors" : ""}`}
-            type="password"
-            id="confirmPassword"
-            name="confirmPassword"
-            placeholder="Confirmare parolă"
-            value={confirmPassword}
-            onChange={(e) => setConfirmPassword(e.target.value)}
-          />
-          {errors.confirmPassword && <small>{errors.confirmPassword}</small>}
-        </div>
-        <button type="submit">Submit</button>
-        <h5>Creează-ți cont!</h5>
-      </form>
-    </div>
+      ) : undefined}
+
+      <div className="signup">
+        <h2>Bun venit!</h2>
+        <br></br>
+        <form onSubmit={handleSubmit}>
+          <div className="form-group">
+            <input
+              className={`${errors.name ? "errors" : ""}`}
+              type="text"
+              id="name"
+              name="name"
+              placeholder="Nume"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+            {errors.name && <small>{errors.name}</small>}
+          </div>
+          <div className="form-group">
+            <input
+              className={`${errors.firstName ? "errors" : ""}`}
+              type="text"
+              id="firstName"
+              name="firstName"
+              placeholder="Prenume"
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+            />
+            {errors.firstName && <small>{errors.firstName}</small>}
+          </div>
+          <div className="form-group">
+            <select
+              id="role"
+              name="role"
+              value={role}
+              onChange={(e) => setRole(e.target.value)}
+            >
+              <option>Organizator</option>
+              <option>Autor</option>
+              <option>Reviewer</option>
+            </select>
+          </div>
+          <div className="form-group">
+            <input
+              className={`${errors.email ? "errors" : ""}`}
+              type="email"
+              id="email"
+              name="email"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            {errors.email && <small>{errors.email}</small>}
+          </div>
+          <div className="form-group">
+            <input
+              className={`${errors.password ? "errors" : ""}`}
+              type="password"
+              id="password"
+              name="password"
+              placeholder="Parolă"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            {errors.password && <small>{errors.password}</small>}
+          </div>
+          <div className="form-group">
+            <input
+              className={`${errors.confirmPassword ? "errors" : ""}`}
+              type="password"
+              id="confirmPassword"
+              name="confirmPassword"
+              placeholder="Confirmare parolă"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+            />
+            {errors.confirmPassword && <small>{errors.confirmPassword}</small>}
+          </div>
+          <button type="submit">Înscrie-te</button>
+          <span onClick={toggle} className="account">
+            Ai deja cont? Conecteaza-te!
+          </span>
+        </form>
+      </div>
+    </>
+  );
+}
+
+function SignIn({ toggle }) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [errors, setErrors] = useState({});
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    setEmail("");
+    setPassword("");
+
+    setErrors({});
+  }
+
+  return (
+    <>
+      <div className="signup">
+        <h2>Bine ai revenit!</h2>
+        <br></br>
+        <form onSubmit={handleSubmit}>
+          <div className="form-group">
+            <input
+              className={`${errors.email ? "errors" : ""}`}
+              type="email"
+              id="email"
+              name="email"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            {errors.email && <small>{errors.email}</small>}
+          </div>
+          <div className="form-group">
+            <input
+              className={`${errors.password ? "errors" : ""}`}
+              type="password"
+              id="password"
+              name="password"
+              placeholder="Parolă"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            {errors.password && <small>{errors.password}</small>}
+          </div>
+          <button type="submit">Înscrie-te</button>
+          <span onClick={toggle} className="account">
+            Nu ai cont? Înscrie-te aici!
+          </span>
+        </form>
+      </div>
+    </>
   );
 }
