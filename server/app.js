6f82a4b4afe488d10dd93ea63170b9f46f370ebb
GeorgianaGavril <georgianagavril401@gmail.com>
2025-01-18 11:32:20 +0200

    adaugat review routes in app.js

--- server/app.js
+++ server/app.js
@@ -1,23 +1,25 @@
-require('dotenv').config({ path: '../.env' });
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config({ path: "../.env" });
+const express = require("express");
+const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 const corsOptions = {
-    origin: 'http://localhost:3000', 
-    methods: ['GET', 'POST', 'PATCH', 'DELETE'],  
-    allowedHeaders: ['Content-Type'], 
+  origin: "http://localhost:3000",
+  methods: ["GET", "POST", "PATCH", "DELETE"],
+  allowedHeaders: ["Content-Type"],
 };
 
-app.use(cors(corsOptions));  
+app.use(cors(corsOptions));
 app.use(express.json());
 
 const userRouter = require("./routes/users");
 const authRouter = require("./routes/auth");
+const reviewRouter = require("./routes/review");
 app.use("/users", userRouter);
 app.use("/", authRouter);
+app.use("/reviews", reviewRouter);
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
